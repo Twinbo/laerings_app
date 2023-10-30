28df5f86c62a7ac433e4d27dc70ec2a1f549a1b4
CheemShiba <mikkel.casselgreen@gmail.com>
2023-10-30 15:46:47 +0100

    Update math.py
    math med plus minus gange

--- math.py
+++ math.py
@@ -1,1 +1,25 @@
-print("Whello world")
+import random
+
+
+
+
+
+#random number 1
+random_number = random.randint(1, 20)
+print(random_number)
+
+#random number 2
+random_number2 = random.randint(1, 20)
+print(random_number2)
+
+
+#math thing
+
+#+
+print(random_number+random_number2)
+
+#-
+print(random_number-random_number2)
+
+#*
+print(random_number*random_number2)
