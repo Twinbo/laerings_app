97e7d96a7d1c6a4eacdce0daba4fbf3e364c9899
Malthe <malthejespersen11@gmail.com>
2023-11-13 15:09:30 +0100

    Implamentering af range, men problemer med at den går videre til minus efter et plus stykke

--- matematikken.py
+++ matematikken.py
@@ -2,62 +2,99 @@ import random
 
 print("start")
 print()
-#random number 1
+
+# random number 1
 random_number = random.randint(1, 20)
 print("første nummer =", random_number)
 
-#random number 2
+# random number 2
 random_number2 = random.randint(1, 20)
 print("anden nummer =", random_number2)
 
-
 print()
 
-#math thing
+# math thing
 
-#+
-class plus:
-    print("plus stykke",random_number,"+",random_number2)
-    svar = int(input())
-    if svar == random_number+random_number2:
-        print("Det er rigtigt, Godt gået")
-    if svar != random_number+random_number2:
-        print("Prøv igen")
-    svar = int(input())
-    if svar == random_number+random_number2:
-        print("Det er rigtigt, godt gået")
-    else:
-        print("svaret er forkert, det rigtige svar var", random_number+random_number2)
+# +
+class Plus:
+    def __init__(self, range_start, range_end):
+        self.range_start = range_start
+        self.range_end = range_end
+        self.random_number = random.randint(range_start, range_end)
+        self.random_number2 = random.randint(range_start, range_end)
 
-#-
-class minus:
-    print("minus stykke",random_number,"-",random_number2)
-    svar = int(input())
-    if svar == random_number-random_number2:
-        print("Det er rigtigt, Godt gået")
-    if svar != random_number-random_number2:
-        print("Prøv igen")
-    svar = int(input())
-    if svar == random_number-random_number2:
-        print("Det er rigtigt, godt gået")
-    else:
-     print("svaret er forkert, det rigtige svar var", random_number-random_number2)
+    def plus_learner(self):
+        print("Plus stykke", self.random_number, "+", self.random_number2)
+        user_answer = int(input())
+        if user_answer == self.random_number + self.random_number2:
+            print("Det er rigtigt, Godt gået")
+        else:
+            print("Prøv igen")
+            user_answer = int(input())
+            if user_answer == self.random_number + self.random_number2:
+                print("Det er rigtigt, godt gået")
+            else:
+                print("Svaret er forkert, det rigtige svar var", self.random_number + self.random_number2)
 
-#*
-class gange:
-    print("gange stykke",random_number,"*",random_number2)
-    svar = int(input())
-    if svar == random_number*random_number2:
-        print("Det er rigtigt, Godt gået")
-    if svar != random_number*random_number2:
-        print("Prøv igen")
-    svar = int(input())
-    if svar == random_number*random_number2:
-     print("Det er rigtigt, godt gået")
-    else:
-     print("svaret er forkert, det rigtige svar var", random_number*random_number2)
+
+range_start = 1
+range_end = 20
+game = Plus(range_start, range_end)
+game.plus_learner()
+
+# -
+class Minus:
+    def __init__(self, range_start, range_end):
+        self.range_start = range_start
+        self.range_end = range_end
+        self.random_number = random.randint(range_start, range_end)
+        self.random_number2 = random.randint(range_start, range_end)
+
+    def minus_learner(self):
+        print("minus stykke", self.random_number, "-", self.random_number2)
+        svar = int(input())
+        if svar == self.random_number - self.random_number2:
+            print("Det er rigtigt, Godt gået")
+        if svar != self.random_number - self.random_number2:
+            print("Prøv igen")
+        svar = int(input())
+        if svar == self.random_number - self.random_number2:
+            print("Det er rigtigt, godt gået")
+        else:
+            print("svaret er forkert, det rigtige svar var", self.random_number - self.random_number2)
+
+range_start = 1
+range_end = 20
+game = Minus(range_start, range_end)
+game.minus_learner()
+
+# *
+class Gange:
+    def __init__(self, range_start, range_end):
+        self.range_start = range_start
+        self.range_end = range_end
+        self.random_number = random.randint(range_start, range_end)
+        self.random_number2 = random.randint(range_start, range_end)
+
+    def gange_learner(self):
+        print("gange stykke", self.random_number, "*", self.random_number2)
+        svar = int(input())
+        if svar == self.random_number * self.random_number2:
+            print("Det er rigtigt, Godt gået")
+        if svar != self.random_number * self.random_number2:
+            print("Prøv igen")
+        svar = int(input())
+        if svar == self.random_number * self.random_number2:
+            print("Det er rigtigt, godt gået")
+        else:
+            print("svaret er forkert, det rigtige svar var", self.random_number * self.random_number2)
+
+range_start = 1
+range_end = 20
+game = Gange(range_start, range_end)
+game.gange_learner()
 
 print("slut")
-#test
 
-
+# Keep the console open until Enter is pressed
+input("Press Enter to exit...")
