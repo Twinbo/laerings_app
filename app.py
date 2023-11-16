79f08adcc0951a9993b82f4c1b920d16ef740273
Remy0204 <142223202+Remy0204@users.noreply.github.com>
2023-11-16 23:16:40 +0100

    Add næsten færdig

--- app.py
+++ app.py
@@ -4,46 +4,66 @@ import tkinter as LP
 from tkinter import *
 import random
 
+K1 = None
+F1 = None
+
 def CreateWindow1():
     Add_Window1 = LP.Toplevel()
     Add_Window1.title('Addition')
     Add_Window1.geometry("800x500")
 
-    def RT():
-        # random number 1
+    def AddRandomTal():
+        global K1
+        global F1
+        
         random_number.set(random.randint(1, 20))
-        print("første nummer =", random_number)
-        # random number 2
         random_number2.set(random.randint(1, 20))
-        print("anden nummer =", random_number2)
+
+        #print("første nummer =", random_number)
+        #print("anden nummer =", random_number2)
 
         SP.config(text=f"{random_number.get()} + {random_number2.get()}")
 
         E1.delete(0, LP.END)
 
-    random_number = LP.StringVar()
-    random_number2 = LP.StringVar() 
+        if K1 is not None:
+            K1.destroy()
+        if F1 is not None:
+            F1.destroy()
 
-    SP = LP.Label(Add_Window1, text="", font=('Arial',44))
-    SP.pack(padx=10,pady=20)
+    random_number = LP.StringVar()
+    random_number2 = LP.StringVar()
+
+    SP = LP.Label(Add_Window1, text="", font=('Arial',80))
+    SP.pack(padx=10,pady=35)
 
     E1 = LP.Entry(Add_Window1,font=('Arial',44),width=8)
     E1.pack(padx=10,pady=10)
 
     def C1():
+        global K1
+        global F1
+
         user_svar = int(E1.get())
         print(user_svar)
 
         if user_svar == int(random_number.get()) + int(random_number2.get()):
-            print("Det er rigtigt, Godt gået1")
+            if F1 is not None:
+                F1.destroy()
+
+            K1 = LP.Label(Add_Window1, text="Korrekt, godt gået", font=('Arial',40))
+            K1.pack(padx=10,pady=10)
+            #print("Det er rigtigt, Godt gået1")
 
         else:
-            print("Svaret er forkert, det rigtige svar var", int(random_number.get()) + int(random_number2.get()))
-
-    B1 = LP.Button(Add_Window1, text="Tjek", font=('Arial', 20), command=C1).pack(padx=10,pady=10)
-    B2 = LP.Button(Add_Window1, text="Næste", font=('Arial', 20),command=RT).pack(padx=10,pady=10)
+            F1 = LP.Label(Add_Window1, text=f"Forkert svar, det rigtige svar er {int(random_number.get()) + int(random_number2.get())}", font=('Arial',40))
+            F1.pack(padx=10,pady=10)
+            #print("Svaret er forkert, det rigtige svar var", int(random_number.get()) + int(random_number2.get()))
+        
+    B1 = LP.Button(Add_Window1, text="Tjek", font=('Arial', 28), command=C1).pack(fill=BOTH)
+    B2 = LP.Button(Add_Window1, text="Næste", font=('Arial', 28),command=AddRandomTal).pack(fill=BOTH)
     
-    RT()
+    AddRandomTal()
     
 def CreateWindow2():
     Add_Window2 = LP.Toplevel()
@@ -61,17 +81,19 @@ def CreateWindow2():
         SP1.config(text=f"{random_number.get()} - {random_number2.get()}")
 
         E2.delete(0, LP.END)
+        
 
     random_number = LP.StringVar()
     random_number2 = LP.StringVar() 
 
     SP1 = LP.Label(Add_Window2, text="", font=('Arial',44))
-    SP1.pack(padx=10,pady=20)
+    SP1.pack(row=3)
 
     E2 = LP.Entry(Add_Window2,font=('Arial',44),width=8)
     E2.pack(padx=10,pady=10)
 
     def C2():
+        
         user_svar = int(E2.get())
         print(user_svar)
 
